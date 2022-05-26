c9e0c34e78a767d80625e81c8cf4d0bc8ab72e29
sindhu <sindhuri.sps@gmail.com>
2022-05-26 12:25:34 +1000

    testrunner

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -7,8 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features",
 plugin = {"pretty","json:target/jsonReports/cucumber-report.json","html:target/cucumber-html-reports/cucumber-report.html"},
-glue = {"stepDefinitions"},
-        tags= "@AddPlace")
+glue = {"stepDefinitions"})
 
 public class TestRunner {
     //, tags= "@AddPlace"
